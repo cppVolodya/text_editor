2cc09a929041392e01dbe92fcf44f414a0318e43
Volodya <dorozovecvoloda@gmail.com>
2022-09-03 23:59:47 +0300

    update

--- text_editor/sources/main.cpp
+++ text_editor/sources/main.cpp
@@ -5,10 +5,10 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication application(argc, argv);
 
     MainWindow window;
     window.show();
 
-    return a.exec();
+    return application.exec();
 }
